1023786ad4572903f0a4ea9498181af051a00c18
Francois M. <francois.marceau@clarifai.com>
2024-10-03 12:19:32 -0400

    reenabling  stuff

--- modules/aws/karpenter.tf
+++ modules/aws/karpenter.tf
@@ -84,11 +84,10 @@ module "karpenter" {
   irsa_oidc_provider_arn          = local.karpenter["irsa_oidc_provider_arn"]
   irsa_namespace_service_accounts = local.karpenter["irsa_namespace_service_accounts"]
 
-  #create_iam_role = true
-  create_iam_role = false
+  create_iam_role = true
   iam_role_name   = local.karpenter["iam_role_name"]
 
-  #create_instance_profile = true
+  create_instance_profile = true
 
   tags = local.tags
 }
